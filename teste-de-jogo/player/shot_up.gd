4f74c799f6295e8278bdc657b27212c53b6e0989
José Eduardo Guimarães Carvalho da Costa <jegcc2007@gmail.com>
2025-12-03 23:10:44 -0300

    Add files via upload

--- teste-de-jogo/player/shot_up.gd
+++ teste-de-jogo/player/shot_up.gd
@@ -1,11 +1,10 @@
 extends Area2D
 
+var speed = 800
+var direction := Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _process(delta):
+	global_position += direction * speed * delta
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func set_direction(new_direction: Vector2): 
+	direction = new_direction.normalized()
